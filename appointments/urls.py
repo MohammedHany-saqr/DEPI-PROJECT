6061ca2564eff4b0082ef05784c5f922d64bbb4d
Mohamed Hany Abdel Monem Saqr <149791832+MohammedHany-saqr@users.noreply.github.com>
2025-11-29 19:06:26 +0200

    Add prediction routes for heart disease and diabetes

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignupPatient, SignupDoctor, Login, ProfileView, DoctorList, CreateAppointment, MyAppointments
+from .views import SignupPatient, SignupDoctor, Login, ProfileView, DoctorList, CreateAppointment, MyAppointments, PredictHeartDisease, PredictDiabetes
 
 urlpatterns = [
     path("signup/patient/", SignupPatient.as_view()),
@@ -9,4 +9,7 @@ urlpatterns = [
     path("doctors/", DoctorList.as_view()),
     path("appointment/create/", CreateAppointment.as_view()),
     path("appointments/mine/", MyAppointments.as_view()),
+
+    path("predict/heart/", PredictHeartDisease.as_view()),
+    path("predict/diabetes/", PredictDiabetes.as_view()),
 ]
